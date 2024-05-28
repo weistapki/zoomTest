be1d946b94f8c3e60a296f2590e49a7d055a3886
Serhii Martyniuk <weistapki@gmail.com>
2024-05-28 18:58:30 +0200

    Initial commit

---
+++ src/main/java/com/example/zoomtest/ZoomTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.zoomtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoomTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ZoomTestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/zoomtest/ZoomTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zoomtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoomTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
